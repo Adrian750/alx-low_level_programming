ae4f130c76a61d1327279787128c279ec5c5420d
Coker Richard Ayodeji <richardayodeji91@gmail.com>
2022-10-13 19:57:06 -0700

    Learning C is exiting!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
 
 int main(void)
-{
+{	
+	/**
+	 *  *main-program entry point.
+	 *   *
+	 *    *Return: 1 on success.
+	 **/
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return 1;
+	return (1);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,11 @@
 int main(void)
 
 {
+	/**
+	 *  *main- Serves as the entry point for the program
+	 *   *
+	 *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+	 **/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
