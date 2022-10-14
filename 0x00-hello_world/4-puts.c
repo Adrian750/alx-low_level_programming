7460ea3472a81c0678fad311bac090ea71940658
Coker Richard Ayodeji <richardayodeji91@gmail.com>
2022-10-13 20:13:06 -0700

    Learning C is exiting!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	/**
+	/*
 	 * main - Serves as the entry point for the program
-	   
+	`* Description: Returna back a value without using the printf function
 	 * Return: By default returns zero if no erros otherwise returns non-zero value.
-	 **/
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
