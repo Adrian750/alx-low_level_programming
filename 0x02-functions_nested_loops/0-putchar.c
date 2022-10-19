677642cc9a68973e472a0da69792115a14ed94a2
Coker Richard Ayodeji <richardayodeji91@gmail.com>
2022-10-19 01:38:32 +0100

    We are here again!

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  main - executed first; prints putchar
+ * return - always success (0)
+ **/
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+}
